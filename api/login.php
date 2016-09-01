2b4c505f352eda350135c2907a5bf0d203c38297
Upendra Singh <upendras@Upendras-MacBook-Pro.local>
2016-09-01 12:03:17 +0530

    removed echo from login api

--- api/login.php
+++ api/login.php
@@ -6,7 +6,7 @@ session_start();
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 // username and password sent from form 
-
+var_dump($_POST);
 $myusername=addslashes($_POST['username']); 
 $mypassword=addslashes($_POST['password']); 
 
