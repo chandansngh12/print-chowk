a7ca059c59a07bdc6468e968fff1d5460090523d
Upendra Singh <upendras@Upendras-MacBook-Pro.local>
2016-09-01 23:20:39 +0530

    changed return values to JSON in  getOrder API

--- api/getOrder.php
+++ api/getOrder.php
@@ -2,15 +2,35 @@
 include("config.php");
 session_start();
 
+$return_arr = array();
+$row_array = array();
+
 $sql="SELECT * FROM orders";
 $result=mysql_query($sql);
-$row=mysql_fetch_array($result);
 $count=mysql_num_rows($result);
 
 // If result matched $myusername and $mypassword, table row must be 1 row
 if($count>1)
 {
-	echo(json_encode($row));
+	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+
+    $row_array['orderId'] = $row['orderId'];
+    $row_array['orderType'] = $row['orderType'];
+    $row_array['productType'] = $row['productType'];
+    $row_array['productTitle'] = $row['productTitle'];
+    $row_array['productName'] = $row['productName'];
+    $row_array['quantity'] = $row['quantity'];
+    $row_array['dimensions'] = $row['dimensions'];
+    $row_array['heightdim'] = $row['heightdim'];
+    $row_array['widthdim'] = $row['widthdim'];
+    $row_array['finish'] = $row['finish'];
+    $row_array['cutting'] = $row['cutting'];
+    $row_array['comment'] = $row['comment'];
+
+    array_push($return_arr,$row_array);
+}
+
+echo json_encode($return_arr);
 }
 else
 {
