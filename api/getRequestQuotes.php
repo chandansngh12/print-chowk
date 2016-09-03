7d619b196db86e6c4ba1ab5e058404750d4ac703
Upendra Singh <upendras@Upendras-MacBook-Pro.local>
2016-09-03 11:01:45 +0530

    Added getRequestQuotes API

---
+++ api/getRequestQuotes.php
@@ -0,0 +1,30 @@
+<?php
+include("config.php");
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+{
+    print_r($_FILE);
+    $_POST = json_decode(file_get_contents('php://input'), true);
+	$order = json_decode($_POST["orders"]);
+
+
+$sql = "INSERT INTO requestQuotes 
+(`orderType`, `productType`, `productTitle`, `productName`, `quantity`,`dimensions`,`heightdim`,`widthdim`,`finish`,`cutting`,`comment`,
+`fileName`,`amount`,`status`,`paymentType`,`city`,`state`,`landmark`,`zipcode`) 
+VALUES ('".$_POST['orderType']."','".$_POST['productType']."','".$_POST['productTitle']."','".$_POST['productName']."','".$_POST['quantity']."',
+'".$_POST['dimensions']."','".$_POST['heightdim']."','".$_POST['widthdim']."','".$_POST['finish']."','".$_POST['cutting']."','".$_POST['comment']."',
+'".$_POST['imageUrl']."','".$_POST['amount']."','".$_POST['orderStatus']."','".$_POST['paymentType']."','".$_POST['city']."','".$_POST['state']."',
+'".$_POST['landmark']."','".$_POST['zipCode']."')";
+
+$result=mysql_query($sql);
+
+if ($result)
+	{
+		echo "success";
+	}
+else{
+	echo "failed";
+	}
+}
+?>
