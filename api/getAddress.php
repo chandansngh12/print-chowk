0403d0c15c06d1c4e8f5f3b42371edb5fa10f60d
Upendra Singh <upendras@Upendras-MacBook-Pro.local>
2016-09-23 00:16:55 +0530

    Added getAddress API

---
+++ api/getAddress.php
@@ -0,0 +1,20 @@
+<?php
+include("config.php");
+session_start();
+
+if (isset($_SESSION['login_status']))
+{
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+{
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    
+       	echo $sql="SELECT * FROM saveAddress  where `userName`='".$_POST['login_user']."'";
+    	$result=mysql_query($sql);
+			if($result){
+			echo "success";
+			}else
+			echo "failed";
+    	}
+}
+
+?>
