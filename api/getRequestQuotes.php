13c87d5404c38c7209f87d7a3934f6fc1a1c5865
Upendra Singh <upendras@Upendras-MacBook-Pro.local>
2016-09-24 19:47:30 +0530

    Changed sql query

--- api/getRequestQuotes.php
+++ api/getRequestQuotes.php
@@ -4,25 +4,40 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
 {
-    print_r($_FILE);
     $_POST = json_decode(file_get_contents('php://input'), true);
 	$order = json_decode($_POST["orders"]);
 
 
-$sql = "INSERT INTO requestQuotes 
-(`orderType`, `productType`, `productTitle`, `productName`, `quantity`,`dimensions`,`heightdim`,`widthdim`,`finish`,`cutting`,`comment`,
-`fileName`,`amount`,`status`,`paymentType`,`city`,`state`,`landmark`,`zipcode`) 
-VALUES ('".$_POST['orderType']."','".$_POST['productType']."','".$_POST['productTitle']."','".$_POST['productName']."','".$_POST['quantity']."',
-'".$_POST['dimensions']."','".$_POST['heightdim']."','".$_POST['widthdim']."','".$_POST['finish']."','".$_POST['cutting']."','".$_POST['comment']."',
-'".$_POST['imageUrl']."','".$_POST['amount']."','".$_POST['orderStatus']."','".$_POST['paymentType']."','".$_POST['city']."','".$_POST['state']."',
-'".$_POST['landmark']."','".$_POST['zipCode']."')";
+
+$sql = "select * from requestQuotes where userId='".$_SESSION['login_user']."'";
 
 $result=mysql_query($sql);
 
-if ($result)
-	{
-		echo "success";
-	}
+if($count>1)
+{
+	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    $row_array['orderId'] = $row['orderId'];
+    $row_array['orderType'] = $row['orderType'];
+    $row_array['productType'] = $row['productType'];
+    $row_array['productTitle'] = $row['productTitle'];
+    $row_array['productName'] = $row['productName'];
+    $row_array['quantity'] = $row['quantity'];
+    $row_array['dimensions'] = $row['dimensions'];
+    $row_array['heightdim'] = $row['heightdim'];
+    $row_array['widthdim'] = $row['widthdim'];
+    $row_array['finish'] = $row['finish'];
+    $row_array['cutting'] = $row['cutting'];
+    $row_array['comment'] = $row['comment'];
+    $row_array['amount'] = $row['amount'];
+    $row_array['status'] = $row['status'];
+    $row_array['paymentType'] = $row['paymentType'];
+    $row_array['city'] = $row['city'];
+    $row_array['state'] = $row['state'];
+    $row_array['landmark'] = $row['landmark'];
+    $row_array['zipcode'] = $row['zipcode'];
+    array_push($return_arr,$row_array);
+}
+}
 else{
 	echo "failed";
 	}
